c70d188323d694bba70042c753775e74573ca170
Arnoldo Alvarez <p3rolix@Users-iMac.attlocal.net>
2021-07-21 16:04:08 -0400

    Both Versions (class component and Functional Component) working good

--- myRickAndMorty_Projects/ram_react/src/components/App.js
+++ myRickAndMorty_Projects/ram_react/src/components/App.js
@@ -6,88 +6,6 @@ import axios from 'axios'
 import PageError from './pageError'
 import PageLoading from './pageLoading'
 
-// class App extends React.Component {
-//     constructor(props) {
-        
-//         super(props)
-//         this.state = {
-//             chars: [],
-//             error: null,
-//             loading: false
-//         }
-//         console.log('1. Loading: at Constructor ' + this.state.loading)        
-//     }
-    
-    
-
-//     componentDidMount(){
-//         console.log('3. Loading: at componentDidMount ' + this.state.loading)
-//         this.setState({ loading:true })
-//         console.log('4. Loading: at componentDidMount ' + this.state.loading)
-//         this.fetchData()
-//         this.intervalId = setInterval(this.fetchData, 5000);
-    
-//     }
-    
-//    fetchData = async () => {
-        
-//         try{
-//             const data  = await axios.get('http://localhost:3001/characters', {},{
-//             })
-//             this.setState({ loading:false, chars: data.data.results }) 
-//             clearInterval(this.intervalId); 
-//         }catch(err){
-//             if(err.response.status === 400){
-//                 this.setState(
-//                     {
-//                         error: 'Server has no responses for Characters',
-//                         loading: false
-//                     })
-                
-//             }
-
-//         }
-        
-//     }
-   
-//     onHandleDelete = (charToDelete) => {
-//         console.log(charToDelete)
-//         const newChars = this.state.chars.filter((_char)=>{
-//             return _char !== charToDelete
-//     })
-
-//         this.setState({chars: newChars})
-//     }
-    
-//     render() {
-//         console.log('2. Loading: at Render ' + this.state.loading)
-//          if (this.state.loading === true){
-//              return <PageLoading loadingMessage={'Loading...'} />
-//          }
-
-//         if (this.state.error){
-//             return(
-//                 <PageError errorMessage={this.state.error}  />
-//             ) 
-//         }
-    
-//         return (    
-//             <div>
-//                 <div className="header">
-//                     My Rick And Morty with React
-//                 </div>
-//                 <CharList 
-//                 characters={ this.state.chars }
-//                 onHandleDelete = { this.onHandleDelete }
-//                 />
-//             </div>
-//         )
-//     }
-
-// }
-
-
-//**********************************************************************
 const App = () => {
 
    
@@ -167,5 +85,87 @@ const App = () => {
 }
 
 export default App
+
+
+
+// class App extends React.Component {
+//     constructor(props) {
+        
+//         super(props)
+//         this.state = {
+//             chars: [],
+//             error: null,
+//             loading: false
+//         }
+//         console.log('1. Loading: at Constructor ' + this.state.loading)        
+//     }
+    
+    
+
+//     componentDidMount(){
+//         console.log('3. Loading: at componentDidMount ' + this.state.loading)
+//         this.setState({ loading:true })
+//         console.log('4. Loading: at componentDidMount ' + this.state.loading)
+//         this.fetchData()
+//         this.intervalId = setInterval(this.fetchData, 5000);
+    
+//     }
+    
+//    fetchData = async () => {
+        
+//         try{
+//             const data  = await axios.get('http://localhost:3001/characters', {},{
+//             })
+//             this.setState({ loading:false, chars: data.data.results }) 
+//             clearInterval(this.intervalId); 
+//         }catch(err){
+//             if(err.response.status === 400){
+//                 this.setState(
+//                     {
+//                         error: 'Server has no responses for Characters',
+//                         loading: false
+//                     })
+                
+//             }
+
+//         }
+        
+//     }
+   
+//     onHandleDelete = (charToDelete) => {
+//         console.log(charToDelete)
+//         const newChars = this.state.chars.filter((_char)=>{
+//             return _char !== charToDelete
+//     })
+
+//         this.setState({chars: newChars})
+//     }
+    
+//     render() {
+//         console.log('2. Loading: at Render ' + this.state.loading)
+//          if (this.state.loading === true){
+//              return <PageLoading loadingMessage={'Loading...'} />
+//          }
+
+//         if (this.state.error){
+//             return(
+//                 <PageError errorMessage={this.state.error}  />
+//             ) 
+//         }
+    
+//         return (    
+//             <div>
+//                 <div className="header">
+//                     My Rick And Morty with React
+//                 </div>
+//                 <CharList 
+//                 characters={ this.state.chars }
+//                 onHandleDelete = { this.onHandleDelete }
+//                 />
+//             </div>
+//         )
+//     }
+
+// }
 
 
